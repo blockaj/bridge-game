06f4d0c4422e459b7207009de09f0dc57351cb9c
Aaron <ajblock@gmail.com>
2015-06-27 20:50:07 +0200

    Modular project structure has begun

---
+++ public/js/client_side.js
@@ -0,0 +1,6 @@
+require('../../bower_components/angular/angular.js');
+var HomeController = require('./controllers/HomeController');
+
+var app = angular.module('bridge', []);
+
+HomeController(app);
