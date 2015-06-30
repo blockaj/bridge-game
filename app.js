1fe8c8158b9a83133b738404a5966a8f3f3848a4
Aaron <ajblock@gmail.com>
2015-06-30 15:38:39 -0400

    More socket.io handshaking to deal

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express'),
 	http = require('http'),
 	io = require('socket.io'),
+	chalk = require('chalk'),
 	gameport = 3000,
 
 	//Verbose is set to true for debugging
@@ -13,8 +14,9 @@ var express = require('express'),
 
 var GameCore = require('./game.core.js');
 var Player = GameCore.Player;
-var GameHandle = new GameCore.GameHandle;
+var GameHandle = GameCore.GameHandle;
 
+var library = chalk.bold.red;
 
 //Express-related setup
 server.listen(gameport);
@@ -28,7 +30,7 @@ app.get('/', function (req, res) {
 app.get('/*', function (req, res, next) {
 	var file = req.params[0];
 	if (verbose) {
-		console.log('\t :: Express :: file requested: ' + file);
+		console.log('\t ' + library(':: Express ::') + ' file requested: ' + file);
 	}
 	res.sendFile(__dirname + '/' + file);
 });
@@ -47,26 +49,37 @@ var connected_players = [];
 sio.sockets.on('connection', function (client) {
 	client.userid = UUID();
 	client.emit('on_connected', {id: client.userid});
-	connected_players.push(new Player(client.userid));
-
-	console.log('\t :: socket.io :: ' + connected_players.length + ' out of 4 connected players');
+	var newPlayer = new Player(client.userid);
+	connected_players.push(newPlayer);
+	console.log('\t' + library(' :: socket.io :: ') + connected_players.length + ' out of 4 connected players');
 	sio.emit('number_player', {numberOfPlayers: connected_players.length});
-
 	if (connected_players.length == 4) {
 		GameHandle.assignTeams(connected_players);
 		GameHandle.dealCards();
+		sio.emit('ready');
+
 	}
-	console.log('\t :: socket.io :: player ' + client.userid + ' connected');
+	client.on('hand_request', function (data) {
+		for (var i = 0; i < 2; i++) {
+			for (var t = 0; t < 2; t++) {
+				var currentPlayer = GameHandle.teams[t].players[i];
+				if (currentPlayer.userId == client.userid) {
+					client.emit('hand', { hand: currentPlayer.hand });
+				}
+			}
+		}
+	});
+	console.log('\t' + library(' :: socket.io :: ') + ' player ' + client.userid + ' connected');
 
 	client.on('disconnect', function () {
 		for (var i = 0; i < connected_players.length; i++) {
 			if (connected_players[i].userId == client.userid) {
-				console.log(connected_players[i]);
-				connected_players = connected_players.splice(i, 1);
-				console.log('\t :: socket.io :: ' + connected_players.length + ' out of 4 connected players');
-				sio.emit('number_player', {numberOfPLayers: connected_players.length});
+				connected_players.splice(i, 1);
+				console.log('\t' + library(' :: socket.io :: ') + connected_players.length + ' out of 4 connected players');
+				sio.emit('number_player', {numberOfPlayers: connected_players.length});
 			}
 		}
-		console.log('\t :: socket.io :: player ' + client.userid + ' disconnected');
+		console.log('\t' + library(' :: socket.io :: ') + 'player ' + client.userid + ' disconnected');
 	});
+
 });
